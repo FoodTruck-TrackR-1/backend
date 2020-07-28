f39dabd32b68e95a59448ea338788fd876c04307
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-07-28 16:40:59 -0400

    created the rest of the tables in the migration

--- data/migrations/20200726173035_foodtruck-001.js
+++ data/migrations/20200726173035_foodtruck-001.js
@@ -7,6 +7,25 @@ exports.up = function(knex) {
         tbl.string('password', 128).notNullable();
         tbl.boolean('is_operator').defaultTo(false);
     })
+    .createTable('trucks', tbl => {
+        tbl.increments();
+        tbl.string('name', 128).notNullable();
+        tbl.string('cuisine_type', 128).notNullable();
+        tbl.string('truck_image', 1000).notNullable();
+    })
+    .createTable('menuItems', tbl => {
+        tbl.increments();
+        tbl.string('item_name', 128).notNullable();
+        tbl.string('item_description', 128).notNullable();
+        tbl.string('item_photo', 1000).notNullable();
+        tbl.integer('item_price').notNullable();
+        
+    })
+    .createTable('favorites', tbl => {
+        tbl.increments();
+        tbl.integer('truck_id').refrences('trucks.id');
+        tbl.integer('diner_id').refrences('users.id');
+    })
 };
 
 exports.down = function(knex) {
